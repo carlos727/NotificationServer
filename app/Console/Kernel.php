cc6a71cf70eb937afba6c9c0824b2114532d66a3
carlos727 <carlos72793@hotmail.com>
2016-04-19 01:51:42 -0500

    Convert to use Heroku PostgreSQL database

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,26 +5,29 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+require 'vendor/autoload.php';
+
+use Carbon\Carbon;
+
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        // Commands\Inspire::class,
-    ];
+	/**
+	 * The Artisan commands provided by your application.
+	 *
+	 * @var array
+	 */
+	protected $commands = [
+		// Commands\Inspire::class,
+	];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')
-        //          ->hourly();
-    }
+	/**
+	 * Define the application's command schedule.
+	 *
+	 * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+	 * @return void
+	 */
+	protected function schedule(Schedule $schedule)
+	{
+		
+	}
 }
