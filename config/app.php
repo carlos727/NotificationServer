3f936f685faac117bf086e649911ebe7fa0765dc
carlos727 <carlos72793@hotmail.com>
2016-04-19 03:03:24 -0500

    Solve Error

--- config/app.php
+++ config/app.php
@@ -147,7 +147,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        'Davibennun\LaravelPushNotification\LaravelPushNotificationServiceProvider',
 
         /*
          * Application Service Providers...
