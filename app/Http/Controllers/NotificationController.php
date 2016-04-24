64b78c93ab6606481703dd57f4eab8514d47a6cf
carlos727 <carlos72793@hotmail.com>
2016-04-24 03:01:37 -0500

    Solve Error in ELSE Clause

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -13,6 +13,17 @@ class NotificationController extends Controller
 {
 	public function store(Request $request){
 		if ($request->has('deviceToken') && $request->has('program') && $request->has('start_at') && $request->has('day')) {
+			switch ($request->input('day')) {
+				case 0:	$day = "Domingo";break;
+				case 1:	$day = "Lunes";break;
+				case 2: $day = "Martes";break;
+				case 3:	$day = "Miércoles";break;
+				case 4:	$day = "Jueves";break;
+				case 5:	$day = "Viernes";break;
+				case 6:	$day = "Sábado";break;
+				default:break;
+			}
+
 			$notifications = DB::table('notifications')
 								->select('id', 'deviceToken', 'program', 'start_at','day')
 								->get();
@@ -35,32 +46,19 @@ class NotificationController extends Controller
 				$notification->day = $request->input('day');
 				$notification->save();
 
-				switch ($notification->day) {
-					case 0:	$day = "Domingo";break;
-					case 1:	$day = "Lunes";break;
-					case 2: $day = "Martes";break;
-					case 3:	$day = "Miércoles";break;
-					case 4:	$day = "Jueves";break;
-					case 5:	$day = "Viernes";break;
-					case 6:	$day = "Sábado";break;
-					default:break;
-				}
-
 				PushNotification::app('notificationServerAndroid')
 					                ->to($notification->deviceToken)
 					                ->send('El programa '.$notification->program.
 					                		' sera recordado a las '.$notification->start_at.
-					                		' del día '.$day.
-					                		'!');
+					                		' del día '.$day.'!');
 
 				return response($notification, 201);
 			} else {
 				PushNotification::app('notificationServerAndroid')
 					                ->to($notification->deviceToken)
-					                ->send('El programa '.$notification->program.
-					                		' ya esta en los contenidos que serán recordados a las '.$notification->start_at.
-					                		' del día '.$day.
-					                		'!');
+					                ->send('El programa '.$request->input('program').
+					                		' ya esta en los contenidos que serán recordados a las '.$request->input('start_at').
+					                		' del día '.$day.'!');
 			}
 		}
 	}
